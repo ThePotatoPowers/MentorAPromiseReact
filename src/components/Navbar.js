bdeafc1a3f28e1a7a3977e627567597f6fab4663
Zidane Karim <zkarim7676@gmail.com>
2024-07-25 19:22:28 -0400

    fixed bars

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,27 @@
-import React from "react";
-import {
-    Nav,
-    NavLink,
-    Bars,
-    NavMenu,
-
-} from "./NavbarElements";
+import React, { useState } from "react";
+import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements";
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <>
             <Nav>
-                <Bars />
-
-                <NavMenu>
-                    <NavLink to="/" >
+                <Bars onClick={toggleMenu} />
+                <NavMenu isOpen={isOpen}>
+                    <NavLink to="/" onClick={() => setIsOpen(false)}>
                         Home
                     </NavLink>
-                    <NavLink to="/counting" >
+                    <NavLink to="/counting" onClick={() => setIsOpen(false)}>
                         Counting
                     </NavLink>
-                    <NavLink to="/counting2" >
+                    <NavLink to="/counting2" onClick={() => setIsOpen(false)}>
                         Counting 2
                     </NavLink>
-
                 </NavMenu>
             </Nav>
         </>
--- src/components/NavbarElements.js
+++ src/components/NavbarElements.js
@@ -8,8 +8,6 @@ export const Nav = styled.nav`
     justify-content: space-between;
     padding: 0.2rem calc((100vw - 1000px) / 2);
     z-index: 12;
-    /* Third Nav */
-    /* justify-content: flex-start; */
 `;
 
 export const NavLink = styled(Link)`
@@ -43,43 +41,13 @@ export const NavMenu = styled.div`
     display: flex;
     align-items: center;
     margin-right: -24px;
-    /* Second Nav */
-    /* margin-right: 24px; */
-    /* Third Nav */
-    /* width: 100vw;
-  white-space: nowrap; */
     @media screen and (max-width: 768px) {
-        display: none;
-    }
-`;
-
-export const NavBtn = styled.nav`
-    display: flex;
-    align-items: center;
-    margin-right: 24px;
-    /* Third Nav */
-    /* justify-content: flex-end;
-  width: 100vw; */
-    @media screen and (max-width: 768px) {
-        display: none;
-    }
-`;
-
-export const NavBtnLink = styled(Link)`
-    border-radius: 4px;
-    background: #808080;
-    padding: 10px 22px;
-    color: #000000;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    text-decoration: none;
-    /* Second Nav */
-    margin-left: 24px;
-    &:hover {
-        transition: all 0.2s ease-in-out;
-        background: #fff;
-        color: #808080;
+        display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+        flex-direction: column;
+        width: 100%;
+        position: absolute;
+        top: 85px;
+        left: 0;
+        background-color: white;
     }
 `;
